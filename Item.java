9cff0c066122ee1e5976716b39c56814acacc193
Marah Zeibak <zeibakmarah@gmail.com>
2021-10-13 10:44:45 +0200

    added Index.txt

--- Item.java
+++ Item.java
@@ -1,3 +1,17 @@
 public class Item {
-  private int item:
+  private String item;
+  private int size;
+
+  public Item (String item, int size) {
+    this.item = item;
+    this.size = size;
+  }
+
+  public String getItem() {
+    return item;
+  }
+
+  public int getSize() {
+    return size;
+  }
 }
