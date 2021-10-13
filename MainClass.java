629435b2482651728e7edfa77e135a12b9298ffe
Marah Zeibak <zeibakmarah@gmail.com>
2021-10-13 10:15:38 +0200

    added Color.java

---
+++ MainClass.java
@@ -0,0 +1,9 @@
+public class MainClass {
+    public static void main(String[] args) {
+        Number number1 = new Number(5);
+
+        int num = 7;
+
+        System.out.println(number1.getNumber() * num);
+    }
+}
---
+++ Number.java
@@ -0,0 +1,11 @@
+public class Number {
+    private int number;
+
+    public Number(int num) {
+        this.number = num;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
